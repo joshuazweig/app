fe57f1e69b03e531dc1483112bf407d718f0abfc
Joshua Zweig <joshuazweig@Joshuas-MacBook-Pro-2.local>
2022-07-05 14:56:14 -0400

    got some stuff working

--- pages/js-form.js
+++ pages/js-form.js
@@ -1,51 +1,56 @@
-export default function PageWithJSbasedForm() {
-    // Handles the submit event on form submit.
-    const handleSubmit = async (event) => {
-      // Stop the form from submitting and refreshing the page.
-      event.preventDefault()
-  
-      // Get data from the form.
-      const data = {
-        first: event.target.first.value,
-        last: event.target.last.value,
-      }
-  
-      // Send the data to the server in JSON format.
-      const JSONdata = JSON.stringify(data)
-  
-      // API endpoint where we send form data.
-      const endpoint = '/api/form'
-  
-      // Form the request for sending data to the server.
-      const options = {
-        // The method is POST because we are sending data.
-        method: 'POST',
-        // Tell the server we're sending JSON.
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        // Body of the request is the JSON data we created above.
-        body: JSONdata,
-      }
-  
-      // Send the form data to our forms API on Vercel and get a response.
-      const response = await fetch(endpoint, options)
-  
-      // Get the response data from server as JSON.
-      // If server returns the name submitted, that means the form works.
-      const result = await response.json()
-      alert(`Is this your full name: ${result.data}`)
-    }
+import React, { Component } from 'react';
+
+class JSForm extends Component {
+  state = {
+    name:''
+  };
+
+  render() {
     return (
-      // We pass the event to the handleSubmit() function on submit.
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="first">First Name</label>
-        <input type="text" id="first" name="first" required />
-  
-        <label htmlFor="last">Last Name</label>
-        <input type="text" id="last" name="last" required />
-  
-        <button type="submit">Submit</button>
-      </form>
-    )
+      <div>
+        <form 
+          onSubmit={async event => {
+            event.preventDefault()
+            this.setState({
+              software_title: event.target.software_title.value,
+              software_version: event.target.software_version.value,
+            });
+
+            const data = {
+              software_title: event.target.software_title.value,
+              software_version: event.target.software_version.value,
+            }
+
+            const JSONdata = JSON.stringify(data)
+            const endpoint = '/api/form'
+
+            const options = {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSONdata,
+            }
+
+            const response = await fetch(endpoint, options) //api call 
+            const result = await response.json()
+            this.setState({req_result: result.data})
+
+
+          }}>
+          <label htmlFor="software_title">Software Title</label>
+          <input type="text" id="software_title" name="software_title" value={this.state.software_title} required />
+    
+          <label htmlFor="software_version">Last Name</label>
+          <input type="text" id="software_version" name="software_version" value={this.state.software_version} required />
+    
+          <button type="submit">Submit</button>
+        </form>
+          <p> Requested Product Lookup: {this.state.software_title} + {this.state.software_version} </p>
+          <p> {this.state.req_result} </p>
+      </div>
+    );
   }
+}
+
+export default JSForm;
